9f00e000d2a232bae0c0c7815ff5cac0a7260bab
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2014-11-19 00:19:52 +0000

    Implement hello url in HelloController with HelloControllerIntegrationTest

--- src/main/java/com/czeczotka/spring/mvc/HelloController.java
+++ src/main/java/com/czeczotka/spring/mvc/HelloController.java
@@ -4,14 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static java.text.MessageFormat.format;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/hello")
 public class HelloController {
     
 	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
+	public String hello(@RequestParam(required = false, defaultValue = "world") String name, ModelMap model) {
+		model.addAttribute("message", format("Hello {0}!", name));
 		return "hello";
 	}
 }
---
+++ src/test/java/com/czeczotka/spring/mvc/HelloControllerIntegrationTest.java
@@ -0,0 +1,42 @@
+package com.czeczotka.spring.mvc;
+
+import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import javax.servlet.http.HttpServletResponse;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration("classpath:mvc-dispatcher-servlet.xml")
+public class HelloControllerIntegrationTest {
+    
+    private final static String HELLO = "/hello";
+    
+    private MockMvc mockMvc;
+    
+    @Autowired
+    private WebApplicationContext context;
+    
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+    }
+    
+    @Test public void 
+    successfulGetRequest() {
+        given().
+                mockMvc(mockMvc).
+        when().
+                get(HELLO).
+        then().
+                statusCode(HttpServletResponse.SC_OK);
+    }   
+}
