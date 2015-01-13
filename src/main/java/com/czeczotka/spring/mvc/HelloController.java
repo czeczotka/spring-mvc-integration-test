3342858d00dff5c3f71b15793311655e0b01c05a
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-01-13 23:06:19 +0000

    Refactor HelloController to return JSON

--- src/main/java/com/czeczotka/spring/mvc/HelloController.java
+++ src/main/java/com/czeczotka/spring/mvc/HelloController.java
@@ -1,25 +1,24 @@
 package com.czeczotka.spring.mvc;
 
+import com.czeczotka.spring.domain.News;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import static java.text.MessageFormat.format;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello")
 public class HelloController {
     
-	@RequestMapping(method = RequestMethod.GET)
-	public @ResponseBody String hello(
-            @RequestParam(required = false, defaultValue = "world") String name, 
-            HttpServletResponse response) {
-        
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	public @ResponseBody String hello(@RequestParam(required = false, defaultValue = "world") String name) {
 		return format("Hello {0}!", name);
+	}
+    
+    @RequestMapping(value = "/latest", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+	public @ResponseBody News getLatestNews() {
+		return new News("Latest news!", "These are the latest news!", null);
 	}
 }
