c2915eca07781446dbaca6b54041723547a32f4e
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-01-14 00:04:05 +0000

    Extract content type constant in HelloController

--- src/main/java/com/czeczotka/spring/mvc/HelloController.java
+++ src/main/java/com/czeczotka/spring/mvc/HelloController.java
@@ -11,13 +11,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    
-	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+
+    private static final String CONTENT_TYPE = "application/json;charset=UTF-8";
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = CONTENT_TYPE)
 	public @ResponseBody String hello(@RequestParam(required = false, defaultValue = "world") String name) {
 		return format("Hello {0}!", name);
 	}
     
-    @RequestMapping(value = "/latest", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @RequestMapping(value = "/latest", method = RequestMethod.GET, produces = CONTENT_TYPE)
 	public @ResponseBody News getLatestNews() {
 		return new News("Latest news!", "These are the latest news!", null);
 	}
