b612c48edf430f78f4522cf6ebbf00659094338a
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-01-16 21:17:40 +0000

    Add controller method and test for getting multiple pieces of news

--- src/main/java/com/czeczotka/spring/web/NewsController.java
+++ src/main/java/com/czeczotka/spring/web/NewsController.java
@@ -4,9 +4,12 @@ import com.czeczotka.spring.domain.News;
 import com.czeczotka.spring.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class NewsController extends AbstractController {
@@ -18,6 +21,11 @@ public class NewsController extends AbstractController {
 	public @ResponseBody News getLatestNews() {
 		return newsService.getLatestNews();
 	}
+
+    @RequestMapping(value = "/latest/{number}", method = RequestMethod.GET, produces = CONTENT_TYPE)
+    public @ResponseBody List<News> getLatestNews(@PathVariable(value = "number") Integer number) {
+        return newsService.getLatestNews(number);
+    }
 
     /*
      *   accessor methods below
--- src/test/java/com/czeczotka/spring/web/NewsControllerIntegrationTest.java
+++ src/test/java/com/czeczotka/spring/web/NewsControllerIntegrationTest.java
@@ -16,6 +16,7 @@ import org.springframework.web.context.WebApplicationContext;
 import javax.servlet.http.HttpServletResponse;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -57,5 +58,45 @@ public class NewsControllerIntegrationTest {
                 body ("headline", equalTo ("Some news!")).
                 body ("article", equalTo ("These are some news!")).
                 body ("timestamp", equalTo (now.toString ()));
+    }
+
+    @Test public void
+    getLatestWithNumber () {
+        int number = 3;
+        LocalDateTime now = LocalDateTime.now ();
+        org.mockito.Mockito.
+                when (newsService.getLatestNews (number)).
+                thenReturn (Arrays.asList (
+                        new News ("Some news! 0", "These are some news! 0", now),
+                        new News ("Some news! 1", "These are some news! 1", now),
+                        new News ("Some news! 2", "These are some news! 2", now)
+                ));
+
+        given ().
+                mockMvc (mockMvc).
+        when ().
+                get (LATEST + "/" + number).
+        then ().
+                statusCode (HttpServletResponse.SC_OK).
+                contentType ("application/json").
+                body ("[0].headline",  equalTo ("Some news! 0")).
+                body ("[0].article",   equalTo ("These are some news! 0")).
+                body ("[0].timestamp", equalTo (now.toString ())).
+                body ("[1].headline",  equalTo ("Some news! 1")).
+                body ("[1].article",   equalTo ("These are some news! 1")).
+                body ("[1].timestamp", equalTo (now.toString ())).
+                body ("[2].headline",  equalTo ("Some news! 2")).
+                body ("[2].article",   equalTo ("These are some news! 2")).
+                body ("[2].timestamp", equalTo (now.toString ()));
+    }
+
+    @Test public void
+    unsuccessfulLatestWithNumber() {
+        given ().
+                mockMvc (mockMvc).
+        when ().
+                get (LATEST + "/abc").
+        then ().
+                statusCode (HttpServletResponse.SC_BAD_REQUEST);
     }
 }
