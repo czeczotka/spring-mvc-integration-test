45289240aedd89138f7869f23af064b1d740d61f
Jakub Czeczótka <jakub.czeczotka@gmail.com>
2015-01-16 21:16:46 +0000

    Add service method for getting multiple pieces of news

--- src/main/java/com/czeczotka/spring/service/NewsService.java
+++ src/main/java/com/czeczotka/spring/service/NewsService.java
@@ -2,8 +2,11 @@ package com.czeczotka.spring.service;
 
 import com.czeczotka.spring.domain.News;
 
+import java.util.List;
+
 public interface NewsService {
 
     News getLatestNews ();
 
+    List<News> getLatestNews (int number);
 }
--- src/main/java/com/czeczotka/spring/service/NewsServiceImpl.java
+++ src/main/java/com/czeczotka/spring/service/NewsServiceImpl.java
@@ -3,11 +3,28 @@ package com.czeczotka.spring.service;
 import com.czeczotka.spring.domain.News;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.LinkedList;
+import java.util.List;
+
 @Service
 public class NewsServiceImpl implements NewsService {
 
     @Override
     public News getLatestNews () {
-        return new News("Latest news!", "These are the latest news!", null);
+        return getNews ("");
+    }
+
+    @Override
+    public List<News> getLatestNews (int number) {
+        List<News> newsList = new LinkedList<> ();
+        for (int i = 0; i < number; i++) {
+            newsList.add (getNews (i + ""));
+        }
+        return newsList;
+    }
+
+    private News getNews (String suffix) {
+        return new News ("Latest news!" + suffix, "These are the latest news!" + suffix, LocalDateTime.now ());
     }
 }
